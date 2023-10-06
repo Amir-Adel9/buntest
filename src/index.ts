796ccf52c8a3e2b32e649f82131a3c5bc4af4ea9
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-06 20:30:46 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .get('/test', () => Bun.file('./src/views/index.html'))
+  .get('/hello', () => 'Hello from Bun & HTMX!')
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
